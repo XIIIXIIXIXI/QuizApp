000c7df3fa7c4985c5b853334c8efd220dae3c17
Martin <s182935@student.dtu.dk>
2022-10-31 12:09:20 +0100

    Dependency Injection AppModule setup

--- app/src/main/java/com/example/quizapp/di/AppModule.kt
+++ app/src/main/java/com/example/quizapp/di/AppModule.kt
@@ -1,8 +1,15 @@
 package com.example.quizapp.di
 
+import com.example.quizapp.data.QuizApi
+import com.example.quizapp.data.QuizRepositoryImpl
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
 
 /**
  * How we construct the dependencies so that dagger can inject the classes.
@@ -10,4 +17,20 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideQuizRepository(
+        api: QuizApi
+    ) = QuizRepositoryImpl(api)
+
+    @Singleton
+    @Provides
+    fun provideQuizApi(): QuizApi {
+        return Retrofit.Builder()
+            .addConverterFactory(MoshiConverterFactory.create())
+            .baseUrl("https://opentdb.com/")
+            .build()
+            .create(QuizApi::class.java)
+    }
 }
