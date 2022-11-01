8d0fe9431b086121f93a50e0943586b28308f132
Martin <s182935@student.dtu.dk>
2022-11-01 20:18:14 +0100

    Theme of app and screen template for Category

---
+++ app/src/main/java/com/example/quizapp/presentation/categoryScreens/CategoryElement.kt
@@ -0,0 +1,8 @@
+package com.example.quizapp.presentation.categoryScreens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CategoryElement(){
+
+}
---
+++ app/src/main/java/com/example/quizapp/presentation/categoryScreens/ChooseCategoryScreen.kt
@@ -0,0 +1,10 @@
+package com.example.quizapp.presentation.categoryScreens
+
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Composable
+@Destination
+fun ChooseCategoryScreen(){
+
+}
