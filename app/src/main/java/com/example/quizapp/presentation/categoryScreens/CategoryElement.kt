53b676200d5c521d6b06332d59490540f9f97142
Martin <s182935@student.dtu.dk>
2022-11-02 21:50:45 +0100

    CategoryElement done, size should be fixed later

--- app/src/main/java/com/example/quizapp/presentation/categoryScreens/CategoryElement.kt
+++ app/src/main/java/com/example/quizapp/presentation/categoryScreens/CategoryElement.kt
@@ -1,28 +1,34 @@
 package com.example.quizapp.presentation.categoryScreens
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.SnackbarDefaults.backgroundColor
+
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight.Companion.Medium
+import androidx.compose.ui.text.capitalize
+import androidx.compose.ui.text.font.FontWeight.Companion.Light
 
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.quizapp.R
+import com.example.quizapp.ui.theme.ColorHistory
+import com.example.quizapp.ui.theme.QuizAppTheme
+import java.util.*
+
 
 /*
 @Composable
@@ -54,30 +60,32 @@ fun CategoryElement() {
 }
 */
 @Composable
-fun CategoryElement() {
+fun CategoryElement(
+    color: Color,
+    category: String
+) {
     Column(
         modifier = Modifier.padding(17.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
             Card(
                 modifier = Modifier
-                    .width(100.dp)
-                    .height(100.dp)
-                    .border(width = 2.dp, color = Color.Cyan, shape = CircleShape),
-                backgroundColor = Color.Blue,
-                shape = CircleShape
+                    .size(100.dp)
+                    .border(width = 2.dp, color = color, shape = CircleShape),
+                shape = CircleShape,
+                backgroundColor = MaterialTheme.colors.background
             ){
                 Column( horizontalAlignment = Alignment.CenterHorizontally) {
                     Image(
-                        painter = painterResource(id = R.drawable.history),
+                        painter = painterResource(id = LocalContext.current.resources.getIdentifier(category, "drawable", LocalContext.current.packageName)),
                         contentDescription = "Circle",
-                        modifier = Modifier.size(70.dp)
+                        modifier = Modifier.size(70.dp).padding(top = 20.dp)
                     )
                     Text(
-                        text = "History",
-                        fontWeight = Medium,
-                        fontSize = 15.sp,
+                        text = category.replaceFirstChar { it.uppercase() },
+                        fontWeight = Light,
+                        fontSize = 10.sp,
+                        color = Color.LightGray,
                         //modifier = Modifier.width(100.dp)
                     )
                 }
@@ -95,5 +103,11 @@ fun CategoryElement() {
 @Composable
 @Preview
 fun CategoryElementPreview(){
-    CategoryElement()
+    QuizAppTheme {
+        Surface(
+            color = MaterialTheme.colors.background
+        ) {
+            CategoryElement(ColorHistory, "history")
+        }
+    }
 }
